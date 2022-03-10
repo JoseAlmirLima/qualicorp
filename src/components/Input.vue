69482afdd1f7d738e1db2ef018d1b4a0360a0b59
Almir LIma <43183680+JoseAlmirLima@users.noreply.github.com>
2022-03-10 00:59:45 -0300

    Update Input.vue

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -14,4 +14,11 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+input {
+  border: 1px solid #c5c5c5;
+  padding: 5px;
+  border-radius: 8px;
+  outline: none;
+}
+</style>
